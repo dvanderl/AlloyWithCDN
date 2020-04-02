ef9716164bc17d4b5e3e46d60469f2175ce8800a
Derek <dvanderl@users.noreply.github.com>
2020-04-02 09:09:45 -0500

    Add project files.

---
+++ AlloyWithCDN/ContentDeliveryApiInitialization.cs
@@ -0,0 +1,36 @@
+﻿using Castle.Components.DictionaryAdapter.Xml;
+using EPiServer.ContentApi.Cms;
+using EPiServer.ContentApi.Core.Configuration;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.ServiceLocation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AlloyWithCDN
+{
+    [ModuleDependency(typeof(ContentApiCmsInitialization))]
+    public class ContentDeliveryApiInitialization : IConfigurableModule
+    {
+        public void Initialize(InitializationEngine context)
+        {
+            //Add initialization logic, this method is called once after CMS has been initialized
+        }
+
+        public void Uninitialize(InitializationEngine context)
+        {
+            //Add uninitialization logic
+        }
+
+        public void ConfigureContainer(ServiceConfigurationContext context)
+        {
+            // set minimumRoles to empty to allow anonymous calls (for visitors to view site in view mode)
+            context.Services.Configure<ContentApiConfiguration>(config =>
+            {
+                config.Default().SetMinimumRoles(string.Empty);
+            });
+        }
+    }
+}
---
+++ AlloyWithCDN/Models/Pages/ISearchPage.cs
@@ -0,0 +1,9 @@
+namespace AlloyWithCDN.Models.Pages
+{
+    /// <summary>
+    /// Marker interface for search implementation
+    /// </summary>
+    public interface ISearchPage
+    {
+    }
+}
